e9c4069508065d199b2ac8edf3180519a6571118
pepijnTEW <37923@ma-web.nl>
2024-03-27 11:20:40 +0100

    mooi maken

--- M3BO/public/assets/index.php
+++ M3BO/public/assets/index.php
@@ -1,16 +1,31 @@
-<?php
-
-
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
 <body>
-    
+    <main>
+        <article class="card" id="js--weer">   
+            weer
+        </article>
+        <article class="card" id="js--tempratuur">
+            tempratuur
+        </article>
+        <article class="card" id="js--uitgavenKWH">
+            uitgavenKWH
+        </article>
+        <article class="card" id="js--uitgavenGas">
+            uitgavenGAS
+        </article>
+        <article class="card" id="js--tijd">
+            tijd
+        </article>
+        <article class="card" id="js--instellingen">
+            instellingen
+        </article>
+    </main>
 </body>
 </html>
